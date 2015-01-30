0389a1e9644f0cd38c9208abf264ea8d4e3ecb4f
Mark Natera <mark@truthkingdom.com>
2015-01-30 00:48:15 -0500

    Desktop consierge complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get '/development' => 'pages#development'
+  get '/concierge' => 'pages#concierge'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
