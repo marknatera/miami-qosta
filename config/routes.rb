05614fd539c44af9602470756f4d7d739e9c85f6
Mark Natera <mark@truthkingdom.com>
2015-01-30 14:57:43 -0500

    Investments page desktop ready

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/executive-concierge' => 'pages#executive'
   get '/exotic-concierge' => 'pages#exotic'
   get '/time-management' => 'pages#time'
+  get '/investments' => 'pages#investments'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
