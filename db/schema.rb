792dd447613be35392ed00120d903634ac716dab
Mark Natera <marknatera@gmail.com>
2015-03-25 08:45:15 -0400

    Created gallery and pictures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325121000) do
+ActiveRecord::Schema.define(version: 20150325123605) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -116,6 +116,23 @@ ActiveRecord::Schema.define(version: 20150325121000) do
     t.string   "floorplan_pdf_content_type"
     t.integer  "floorplan_pdf_file_size"
     t.datetime "floorplan_pdf_updated_at"
+  end
+
+  create_table "galleries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "gallery_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "residences", force: :cascade do |t|
