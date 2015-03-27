eaa2a016dceac653e5caeaa51c153579608553f4
Mark Natera <marknatera@gmail.com>
2015-03-27 01:03:15 -0400

    Removed some stuff from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'developments/new_developments'
-
-  get 'developments/existing_developments'
-
-  get 'developments/residences'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
