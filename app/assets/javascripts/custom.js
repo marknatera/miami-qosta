3d249c5e69d77bc32a53a14421a678579bea5cbd
Mark Natera <marknatera@gmail.com>
2015-03-30 17:11:03 -0400

    Testing js 13

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -15,18 +15,27 @@ $(function() {
 });
 
 
-
-  var slider = new MasterSlider();
-  slider.setup('masterslider' , {
-          width:1920,    // slider standard width
-          height:1080,   // slider standard height
-          space:5
-          // more slider options goes here...
-          // check slider options section in documentation for more options.
-      });
-  // adds Arrows navigation control to the slider.
-  slider.control('arrows');
-
+  // 
+  // var slider = new MasterSlider();
+  // slider.setup('masterslider' , {
+  //         width:1920,    // slider standard width
+  //         height:1080,   // slider standard height
+  //         space:5
+  //         // more slider options goes here...
+  //         // check slider options section in documentation for more options.
+  //     });
+  // // adds Arrows navigation control to the slider.
+  // slider.control('arrows');
+  $('#masterslider').masterslider({
+      width:1920,    // slider standard width
+      height:1080,   // slider standard height
+      // more options...
+      controls : {
+          arrows : {autohide:false},
+          bullets : {}
+          // more slider controls...
+      }
+  });
 
 // Parallax
 // Home Page
