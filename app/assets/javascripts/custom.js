10c11f318d12bdd3b52f4bef097705b05b99f3e7
Mark Natera <marknatera@gmail.com>
2015-03-30 17:14:28 -0400

    Testing js 14

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -15,7 +15,7 @@ $(function() {
 });
 
 
-  // 
+  //
   // var slider = new MasterSlider();
   // slider.setup('masterslider' , {
   //         width:1920,    // slider standard width
@@ -26,16 +26,6 @@ $(function() {
   //     });
   // // adds Arrows navigation control to the slider.
   // slider.control('arrows');
-  $('#masterslider').masterslider({
-      width:1920,    // slider standard width
-      height:1080,   // slider standard height
-      // more options...
-      controls : {
-          arrows : {autohide:false},
-          bullets : {}
-          // more slider controls...
-      }
-  });
 
 // Parallax
 // Home Page
