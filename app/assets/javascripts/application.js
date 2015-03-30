3f304a84c9ea3f7e0c918b352b49b560644f163e
Mark Natera <marknatera@gmail.com>
2015-03-30 16:34:49 -0400

    Testing js 9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require custom
@@ -37,3 +37,4 @@
 //= require modernizr.custom-loader
 //= require pathLoader
 //= require masterslider
+//= require slider.js
