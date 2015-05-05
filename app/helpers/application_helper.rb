0b61caa23be766bf2c87594a8f867278ec4efc50
Mark Natera <marknatera@gmail.com>
2015-05-05 17:26:59 -0400

    Added meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,12 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+  def meta_description(page_description)
+    content_for(:meta_description) { page_description }
+  end
+
+  def keyword(page_keyword)
+    content_for(:keyword) { page_keyword }
+  end
   
 end
